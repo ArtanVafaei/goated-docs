4b4d44f79e193cde1e43a5be1b21d93913b9d85f
Artan Vafaei <artan.vafaei@yahoo.com>
2025-01-16 12:05:49 -0600

    Add template gallery to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import { Navbar } from "./navbar";
+import { TemplateGallery } from "./template-gallery";
 
 const Home = () => {
   return ( 
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
         <Navbar />
+      </div>
+      <div className="mt-16">
+        <TemplateGallery />
       </div>
       <div className="mt-16">
         Click&nbsp;<Link href="/documents/123">
