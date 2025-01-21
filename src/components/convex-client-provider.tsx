89b60e64c440ec8219d1b06d3dccfe935a73744e
Artan Vafaei <artan.vafaei@yahoo.com>
2025-01-21 11:07:08 -0600

    Add authentication loading screen

--- src/components/convex-client-provider.tsx
+++ src/components/convex-client-provider.tsx
@@ -4,6 +4,7 @@ import { ConvexReactClient, Authenticated, Unauthenticated, AuthLoading } from "
 import { ReactNode } from "react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ClerkProvider, useAuth, SignIn } from "@clerk/clerk-react";
+import { FullscreenLoader } from "./fullscreen-loader";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -23,7 +24,7 @@ export function ConvexClientProvider({ children }: { children: ReactNode }) {
           </div>
         </Unauthenticated>
         <AuthLoading>
-          <p>Loading authenticator...</p>
+          <FullscreenLoader label="Loading authentication..." />
         </AuthLoading>
       </ConvexProviderWithClerk>
     </ClerkProvider>
