827d7185f43cb97b3a5ee949db48c4d140851ad6
Artan Vafaei <artan.vafaei@yahoo.com>
2025-01-23 18:11:16 -0600

    Add functionality to create documents in database from home page

--- src/app/(home)/template-gallery.tsx
+++ src/app/(home)/template-gallery.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useMutation } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useState } from "react";
 import {
   Carousel,
   CarouselContent,
@@ -11,7 +15,20 @@ import { templates } from "@/constants/templates";
 import { cn } from "@/lib/utils";
 
 export const TemplateGallery = () => {
-  const isCreating = false;
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+  const [isCreating, setIsCreating] = useState(false);
+
+  const onTemplateClick = (title: string, initialContent: string) => {
+    setIsCreating(true);
+    create({ title, initialContent })
+      .then((documentId) => {
+        router.push(`/documents/${documentId}`);
+      })
+      .finally(() => {
+        setIsCreating(false);
+      });
+  };
 
   return (
     <div className="bg-[#F1F3F4]">
@@ -32,7 +49,8 @@ export const TemplateGallery = () => {
                 >
                   <button
                     disabled={isCreating}
-                    onClick={() => {}}
+                    // TODO: Add initial content
+                    onClick={() => onTemplateClick(template.label, "")}
                     style={{
                       backgroundImage: `url(${template.imageUrl})`,
                       backgroundSize: "cover",
