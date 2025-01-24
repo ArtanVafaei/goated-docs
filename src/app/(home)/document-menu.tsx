592e228295061f410dfc459ee37ebd9aa118d4d4
Artan Vafaei <artan.vafaei@yahoo.com>
2025-01-24 16:40:26 -0600

    Add document dropdown menu to home page and add open in new tab function

---
+++ src/app/(home)/document-menu.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { MoreVertical, SquareArrowOutUpRightIcon } from "lucide-react";
+import { Id } from "../../../convex/_generated/dataModel";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+interface DocumentMenuProps {
+  documentId: Id<"documents">;
+  title: string;
+  onNewTab: (id: Id<"documents">) => void;
+};
+
+export const DocumentMenu = ({ documentId, title, onNewTab }: DocumentMenuProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" className="rounded-full">
+          <MoreVertical className="size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem
+          onClick={() => onNewTab(documentId)}
+          className="cursor-pointer"
+        >
+          <SquareArrowOutUpRightIcon className="size-4 mr-2" />
+          Open in new tab
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+};
