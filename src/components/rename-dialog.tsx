3e166f31a0c8aa6fb648516367290fecf097796e
Artan Vafaei <artan.vafaei@yahoo.com>
2025-01-30 00:56:13 -0600

    Add notifications for creating, renaming, and deleting documents

--- src/components/rename-dialog.tsx
+++ src/components/rename-dialog.tsx
@@ -13,6 +13,7 @@ import { api } from "../../convex/_generated/api";
 import { useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { toast } from "sonner";
 
 interface RenameDialogProps {
   documentId: Id<"documents">;
@@ -32,9 +33,11 @@ export const RenameDialog = ({ documentId, initialTitle, children }: RenameDialo
     setIsUpdating(true);
 
     update({ id: documentId, title: title.trim() || "Untitled" })
-      .then(() => setOpen(false))
+      .catch(() => toast.error("Something went wrong"))
+      .then(() => toast.success("Document renamed"))
       .finally(() => {
         setIsUpdating(false)
+        setOpen(false);
       });
   };
 
