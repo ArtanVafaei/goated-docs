8b7d14d00a50cecadd93a0c824320af0cbbb4793
Artan Vafaei <artan.vafaei@yahoo.com>
2025-02-01 03:16:04 -0600

    Set up collaborative text editor and add Liveblocks room

---
+++ src/app/documents/[documentId]/room.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+import { useParams } from "next/navigation";
+
+export function Room({ children }: { children: ReactNode }) {
+  const params = useParams();
+
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_KEi04FvA-9JNigiS0BnDcFMO_06o8btjWFJTcVFTX_JQw2CCnaDmVSSDpef6l4CV"}>
+      <RoomProvider id={params.documentId as string}>
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
