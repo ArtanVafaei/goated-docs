830892593f945d246186f9bc848b47f65fb24eeb
Artan Vafaei <artan.vafaei@yahoo.com>
2025-03-23 23:43:26 -0500

    Add document title update functionality

--- src/app/documents/[documentId]/document-input.tsx
+++ src/app/documents/[documentId]/document-input.tsx
@@ -1,8 +1,10 @@
 import { BsCloudCheck } from "react-icons/bs";
 import { Id } from "../../../../convex/_generated/dataModel";
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { api } from "../../../../convex/_generated/api";
 import { useMutation } from "convex/react";
+import { useDebounce } from "@/hooks/use-debounce";
+import { toast } from "sonner";
 
 interface DocumentInputProps {
   title: string;
@@ -16,18 +18,42 @@ export const DocumentInput = ({ title, id }: DocumentInputProps) => {
   const [isEditing, setIsEditing] = useState(false);
 
   const inputRef = useRef<HTMLInputElement>(null);
+
   const mutate = useMutation(api.documents.updateById);
+
+  const debouncedUpdate = useDebounce((newValue: string) => {
+    if (newValue === title) return;
+
+    setIsPending(true);
+    mutate({ id, title: newValue })
+      .then(() => toast.success("Document title updated"))
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => setIsPending(false));
+  });
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
     setValue(newValue);
-    // TOOD: Debounced value
+    debouncedUpdate(newValue);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsPending(true);
+    mutate({ id, title: value })
+      .then(() => {
+        toast.success("Document title updated");
+        setIsEditing(false);
+      })
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => setIsPending(false));
   };
 
   return (
     <div className="flex items-center gap-2">
       {isEditing ? (
-        <form className="relative w-fit max-w-[50ch]">
+        <form onSubmit={handleSubmit} className="relative w-fit max-w-[50ch]">
           <span className="invisible whitespace-pre px-1.5 text-lg">
             {value || " "}
           </span>
@@ -35,6 +61,7 @@ export const DocumentInput = ({ title, id }: DocumentInputProps) => {
             ref={inputRef}
             value={value}
             onChange={onChange}
+            onBlur={() => setIsEditing(false)}
             className="absolute inset-0 text-lg text-black px-1.5 bg-transparent truncate"
           />
         </form>
